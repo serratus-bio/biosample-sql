06d254240cf71e8508cac7cb5866fe547a522350
Victor Lin <victor@victorl.in>
2021-02-15 14:08:19 -0500

    deploy manager

--- src/deploy.sh
+++ src/deploy.sh
@@ -6,10 +6,10 @@ aws lambda update-function-code \
 rm package.zip
 popd
 
-# pushd manager
-# zip -rq package .
-# aws lambda update-function-code \
-#     --function-name serratus-summary-uploader-manager \
-#     --zip-file fileb://./package.zip
-# rm package.zip
-# popd
+pushd manager
+zip -rq package .
+aws lambda update-function-code \
+    --function-name biosample-upload-manager \
+    --zip-file fileb://./package.zip
+rm package.zip
+popd
