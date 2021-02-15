d79486b14f006fe6abfee84a0da395c77cd8f85b
Victor Lin <victor@victorl.in>
2021-02-15 14:09:47 -0500

    update manager for lambda

--- src/manager/manager.py
+++ src/manager/manager.py
@@ -1,9 +1,21 @@
-from worker import handler
+from lambda_helpers import invoke_lambda
 import boto3
+
 s3 = boto3.resource('s3')
 s3_object = s3.Object(bucket_name='serratus-public', key='notebook/210212_geo/victorlin/biosample_parse/biosample_set.xml')
 
+WORKER_LAMBDA = 'biosample-upload-worker'
 MAX_ITEMS_PER_WORKER = 1000
+
+
+def handler(event, context):
+    start_byte = event['start_byte']
+    end = s3_object.content_length / 5000
+    while start_byte < end:
+        # print(start_byte)
+        start_byte = prepare_worker(start_byte)
+        print(start_byte / end)
+
 
 def get_contents(start_byte, end_byte=None):
     if not end_byte:
@@ -30,18 +42,11 @@ def prepare_worker(start_byte):
             break
 
     end_byte = start_byte + n_bytes
-    print(f'process({start_byte}, {end_byte})')
+    print(start_byte, end_byte)
     invoke_worker(start_byte, end_byte)
     return end_byte
 
-def manage():
-    start_byte = 0
-    end = s3_object.content_length / 5000
-    while start_byte < end:
-        # print(start_byte)
-        start_byte = prepare_worker(start_byte)
-        print(start_byte / end)
 
 def invoke_worker(start_byte, end_byte):
-    pass
-    # handler({'start_byte': start_byte, 'end_byte': end_byte}, None)
+    event = {'start_byte': start_byte, 'end_byte': end_byte}
+    # invoke_lambda(WORKER_LAMBDA, event)
