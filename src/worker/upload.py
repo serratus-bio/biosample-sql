735da14c4c2aff407cfb54ba017877be3b6b9563
Victor Lin <victor@victorl.in>
2021-02-15 13:53:44 -0500

    fix sqlalchemy dialect issue on lambda

--- src/worker/upload.py
+++ src/worker/upload.py
@@ -1,4 +1,7 @@
 from sqlalchemy import create_engine
+from sqlalchemy_aurora_data_api import register_dialects
+
+register_dialects()
 
 cluster_arn = 'arn:aws:rds:us-east-1:797308887321:cluster:serratus-aurora'
 secret_arn = 'arn:aws:secretsmanager:us-east-1:797308887321:secret:rds-db-credentials/cluster-KOFPN4Q2TKDBO5FHY6QO5M3S7Q/serratus-agdBn9'
