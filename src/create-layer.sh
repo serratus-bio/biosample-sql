c171b6f4b58501d883bc15e946cfe2125037a048
Victor Lin <victor@victorl.in>
2021-02-15 13:33:43 -0500

    use s3 for lambda layer upload

--- src/create-layer.sh
+++ src/create-layer.sh
@@ -12,11 +12,13 @@ zip -r9 -q ../layer.zip python
 popd
 rm -r tmp ve
 
+aws s3 cp layer.zip s3://serratus-aurora/lambda/layer.zip
+
 aws lambda publish-layer-version \
     --layer-name biosample-upload \
-    --zip-file fileb://./layer.zip \
+    --content S3Bucket=serratus-aurora,S3Key=lambda/layer.zip \
     --compatible-runtimes python3.7
 
 aws lambda update-function-configuration \
     --function-name  biosample-upload-worker \
-    --layers "arn:aws:lambda:us-east-1:797308887321:layer:biosample-upload:6"
+    --layers "arn:aws:lambda:us-east-1:797308887321:layer:biosample-upload:8"
