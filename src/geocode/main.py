b3153f7d41316947ba69fdeba3587d6dbcbcef20
Victor Lin <victor@victorl.in>
2021-02-21 09:51:29 -0500

    minimal pickling comments

--- src/geocode/main.py
+++ src/geocode/main.py
@@ -12,12 +12,10 @@ df = download(query)
 df['coordinate_x'], df['coordinate_y'] = zip(*df['geo_text_extracted'].map(get_coords))
 upload(df)
 
-# pickled
 
-# df.to_pickle('geo_text_extracted_distinct.pickle')
+# pickle
+
+# df.to_pickle('geo_text_extracted_geocoded.pickle')
 # import pickle
 # import pandas as pd
-# df = pd.read_pickle('geo_text_extracted_distinct.pickle')
-# df_head = df[:100].copy()
-# for geo_text in df_head.geo_text_extracted:
-#     print(get_coords(geo_text))
+# df = pd.read_pickle('geo_text_extracted_geocoded.pickle')
