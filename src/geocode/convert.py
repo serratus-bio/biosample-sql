f03f968193f57cf997ec028382ee42418e1be5a3
Victor Lin <victor@victorl.in>
2021-02-21 17:16:27 -0500

    remove NLP from geocoding, keep code for reference

--- src/geocode/convert.py
+++ src/geocode/convert.py
@@ -1,9 +1,7 @@
-from text_nlp import clean_geo_text
 import boto3
 client = boto3.client('location')
 
 def get_coords(text):
-    text = clean_geo_text(text)
     if len(text) > 200:
         text = text[:200]
     if text.isdigit():
--- src/geocode/text_nlp.py
+++ src/geocode/text_nlp.py
@@ -1,30 +1,32 @@
+# This module is unused by the geocoding step.
+# NLP should be used at the extraction step so we can get clean data for geocoding
+
 from nltk import word_tokenize, pos_tag, ne_chunk
 from nltk import Tree
 import truecase
 
 
-# from https://stackoverflow.com/a/48676189/4410590
-def get_continuous_chunks(text, label):
-    chunked = ne_chunk(pos_tag(word_tokenize(text)))
-    prev = None
-    continuous_chunk = []
-    current_chunk = []
+node_types = {'NN', 'NNS', 'NNP', 'CD', ':', '.', ','}
+geo_labels = {'GPE', 'PERSON', 'ORGANIZATION'}
 
-    for subtree in chunked:
-        if type(subtree) == Tree and subtree.label() == label:
-            current_chunk.append(" ".join([token for token, pos in subtree.leaves()]))
-        if current_chunk:
-            named_entity = " ".join(current_chunk)
-            if named_entity not in continuous_chunk:
+# derived from https://stackoverflow.com/a/48676189/4410590
+def get_continuous_chunks(text):
+    chunked = ne_chunk(pos_tag(word_tokenize(text)))
+    continuous_chunk = []
+
+    for item in chunked:
+        if type(item) == Tree:
+            subtree = item
+            if subtree.label() in geo_labels:
+                named_entity = " ".join([token for token, pos in subtree.leaves()])
                 continuous_chunk.append(named_entity)
-                current_chunk = []
-        else:
-            continue
+        elif item[1] in node_types:
+            continuous_chunk.append(item[0])
 
     return continuous_chunk
 
 
 def clean_geo_text(text):
     text_cased = truecase.get_true_case(text)
-    geo_text = get_continuous_chunks(text_cased, 'GPE')
+    geo_text = get_continuous_chunks(text_cased)
     return ' '.join(geo_text)
