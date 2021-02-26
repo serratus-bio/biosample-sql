1798183a286c0a2dd2bc039c06ac598c56c281cd
Victor Lin <13424970+victorlin@users.noreply.github.com>
2021-02-25 23:14:20 -0500

    Add coordinate parsing boundaries, use staging tables, remove notebook (#7)
    - add coordinate parsing boundaries
    - use staging tables `biosample2`, `biosample_geocode2`
    - move notebook+html plots to https://github.com/victorlin/serratus-geo-plots

--- src/extract_attributes/worker/upload.py
+++ src/extract_attributes/worker/upload.py
@@ -6,7 +6,7 @@ register_dialects()
 
 cluster_arn = 'arn:aws:rds:us-east-1:797308887321:cluster:serratus-aurora'
 secret_arn = 'arn:aws:secretsmanager:us-east-1:797308887321:secret:rds-db-credentials/cluster-KOFPN4Q2TKDBO5FHY6QO5M3S7Q/serratus-agdBn9'
-table_name = 'biosample3'
+table_name = 'biosample2'
 engine = create_engine('postgresql+auroradataapi://:@/summary',
             connect_args=dict(aurora_cluster_arn=cluster_arn, secret_arn=secret_arn))
 
